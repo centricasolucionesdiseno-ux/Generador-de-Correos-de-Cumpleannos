6ea71c2bdb1d3adf176f2946d553629bdffba0d6
centricasolucionesdiseno-ux <centricasolucionesdiseno@gmail.com>
2026-03-31 07:52:43 -0500

    Add files via upload

---
+++ datos_banners.js
@@ -0,0 +1,17 @@
+// Fichero: datos_banners.js
+// El equipo de Marketing solo debe editar las URLs que están dentro de las comillas.
+
+const bannerLinks = {
+  "general": "Page off service"
+
+};
+
+
+
+
+
+
+
+
+
+
